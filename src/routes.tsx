cabf125694cafb3dcbe3590afd4af3b30323b3fb
daniel.padilha <danielpadilhapessoal@outlook.com>
2020-11-25 23:20:56 -0300

    fix: general adjustments on config

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,14 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
-const Routes = () => (
+// Project imports
+
+import HomePage from './pages/Home'
+
+const Routes: React.FC = () => (
     <BrowserRouter>
       <Switch>
-        <Route path="/:repository" component={} />
-        <Route path="/" exact component={} />
+        <Route path="/" exact component={HomePage} />
       </Switch>
     </BrowserRouter>
 )
