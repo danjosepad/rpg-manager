ac032cea8ccbbcc8151edcddaf92286c8ae2acab
daniel.padilha <danielpadilhapessoal@outlook.com>
2020-12-30 18:43:39 -0300

    feat: add inputs on Settings

--- src/pages/Authorized/Settings/index.tsx
+++ src/pages/Authorized/Settings/index.tsx
@@ -4,10 +4,14 @@ import React from 'react'
 // Project imports
 
 import UserPicture from 'components/UserPicture'
+import { H3 as Name, H2 } from 'styles/fonts'
+import Input from 'components/Input'
+import Button from 'components/Button'
 
 // Styled components
 
 import { Container, Wrapper, UserInfo, UserData } from './styles'
+
 
 interface SetttingsProps {
 
@@ -18,9 +22,21 @@ const Settings: React.FC<SetttingsProps> = () => {
     <Container>
       <Wrapper>
         <UserInfo>
-          <UserPicture size="140" fullName="Daniel Padilha" />
+          <UserPicture size="100" fullName="Daniel Padilha" />
+          <Name>Daniel José Padilha</Name>
         </UserInfo>
-        <UserData></UserData>
+        <UserData>
+          <H2>Confirme seus dados</H2>
+          <Input
+            label="E-mail"
+            value="Teste"
+          />
+          <Input
+            label="Nome"
+            value="Teste"
+          />
+          <Button text="Alterar sua senha"/>
+        </UserData>
       </Wrapper>
     </Container>
   )
--- src/pages/Authorized/Settings/styles.ts
+++ src/pages/Authorized/Settings/styles.ts
@@ -6,22 +6,31 @@ import { colors } from 'styles/theme'
 export const Container = styled.div`
   background: ${colors.black};
   height: 100%;
+  display: flex;
+  justify-content: center;
 `
 
 export const Wrapper = styled.div`
   display: flex;
   width: 100%;
-  max-width: 600px;
+  max-width: 1200px;
   justify-content: center;
+
+  & > div + div {
+    margin-left: 40px;
+  }
 
 `
 export const UserInfo = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  max-width: 300px;
 `
 
 export const UserData = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   padding: 24px;
 `
