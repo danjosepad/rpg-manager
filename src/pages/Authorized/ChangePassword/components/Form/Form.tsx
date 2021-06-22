eb77d1f250b887396889fd5f6a5e79bc98b27270
daniel.padilha <danielpadilhapessoal@outlook.com>
2021-06-21 23:57:08 -0300

    refactor: add Form component on new file

---
+++ src/pages/Authorized/ChangePassword/components/Form/Form.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Formik } from 'formik'
+
+// Project imports
+
+import Button from 'components/Button'
+import Input from 'components/Input'
+import { FormProps } from './Form.props'
+import { validationSchema } from './Form.schema'
+
+// Styled components
+
+import { Container } from './styles'
+
+const Form = ({ onSubmit }: FormProps) => {
+  const initialValues = {
+    password: '',
+    confirmPassword: ''
+  }
+  
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Container data-testid="form__container">
+        <Input name="password" label="Senha" aria-labelledby="password" />
+        <Input name="confirmPassword" label="Confirmar senha" aria-labelledby="confirm password"/>
+        <Button text="Alterar senha" />
+      </Container>
+    </Formik>
+  )
+}
+
+
+export default Form
